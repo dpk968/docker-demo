aeae7a1d24f930116092296c6e77fdda3b03481e
Deepak Gupta <76945949+dpk968@users.noreply.github.com>
2025-02-19 04:47:26 -0800

    push

---
+++ docker-demo/src/main/java/com/dpk/controller/MyController.java
@@ -0,0 +1,17 @@
+package com.dpk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class MyController {
+	
+	@GetMapping("/")
+	public String getMethodName() {
+		return new String("hello i'm running in docker");
+	}
+	
+
+}
