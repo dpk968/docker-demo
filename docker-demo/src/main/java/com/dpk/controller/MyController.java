aadbd8d9f77f5e04d4f41a071a526bbd7f93b1b9
Deepak Gupta <76945949+dpk968@users.noreply.github.com>
2025-02-20 02:19:59 -0800

    Update MyController.java

--- docker-demo/src/main/java/com/dpk/controller/MyController.java
+++ docker-demo/src/main/java/com/dpk/controller/MyController.java
@@ -1,11 +1,11 @@
 package com.dpk.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-@Controller
+@RestController
 public class MyController {
 	
 	@GetMapping("/")
